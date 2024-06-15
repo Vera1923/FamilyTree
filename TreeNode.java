34f269e755cb5ee503f9cc4260eb8e328db021b0
Vera1923 <151830901+Vera1923@users.noreply.github.com>
2024-06-15 21:09:25 +0300

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TreeNode<T> extends Serializable {
+    void setId(long id);
+    long getId();
+    T getFather();
+    T getMother();
+    boolean addChild(T human);
+    boolean addParent(T human);
+    String getName();
+    LocalDate getDeathDate();
+    LocalDate getBirthDate();
+    List<T> getParents();
+    List<T> getChildren();
+
+}
